cb2f16d75544ba974aa827bd942aa8edc48e6126
王臣 <965242435@qq.com>
2022-09-12 11:25:51 +0800

    @update:Bean获取、返回数据格式（json/xml）

---
+++ src/main/java/geektime/spring/springbucks/getJsonOrXMLData/JsonOrXmlController.java
@@ -0,0 +1,36 @@
+package geektime.spring.springbucks.getJsonOrXMLData;
+
+import geektime.spring.springbucks.service.CoffeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class JsonOrXmlController {
+
+    @Autowired
+    private CoffeeService coffeeService;
+
+
+    /**
+     * 返回json格式数据
+     */
+    @GetMapping(value = "/json")
+    @ResponseBody
+    public String getJson(@RequestBody Map map) {
+       return coffeeService.getCoffeeList(map).toString();
+    }
+
+    /**
+     * 返回xml格式数据
+     */
+    @GetMapping(value = "/json", produces = "application/xml;charset=utf-8")
+    @ResponseBody
+    public String getXml(@RequestBody Map map) {
+        return coffeeService.getCoffeeList(map).toString();
+    }
+}
